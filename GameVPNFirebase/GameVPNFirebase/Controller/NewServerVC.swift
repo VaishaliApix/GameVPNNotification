6d04f43bc4d52e179023a05916e6bbc2080ed21f
Vaishali Rathod <vaishali.rathod@aipxperts.com>
2024-06-17 11:24:14 +0530

    new vpn server add

--- GameVPNFirebase/GameVPNFirebase/Controller/NewServerVC.swift
+++ GameVPNFirebase/GameVPNFirebase/Controller/NewServerVC.swift
@@ -6,24 +6,64 @@
 //
 
 import UIKit
+import SVProgressHUD
 
 class NewServerVC: UIViewController {
-
+    
+    //MARK: - @IBOutlet
+    @IBOutlet weak var txtContry: UITextField!
+    @IBOutlet weak var txtContrycode: UITextField!
+    @IBOutlet weak var txtState: UITextField!
+    @IBOutlet weak var txtUser: UITextField!
+    @IBOutlet weak var txtDomain: UITextField!
+    @IBOutlet weak var txtIPAddress: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var btnsubmit: UIButton!
+    
+    //MARK: - Variable
+    let firebaseConnect = FirebaseConnect()
+    
+    //MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.setup()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+//MARK: - Button Action
+extension NewServerVC{
+    func setup(){
+        self.btnsubmit.layer.cornerRadius = self.btnsubmit.frame.height / 2
+    }
+}
+
+//MARK: - Button Action
+extension NewServerVC{
+    @IBAction func btnSubmitAction(_ sender: UIButton) {
+        if txtUser.text == "" && txtUser.text?.count ?? 0 <= 0{
+            showAlert(title: "GameVPN", message: "Please enter username")
+        }else if txtContry.text == "" {
+            showAlert(title: "GameVPN", message: "Please enter country")
+        }else if txtContrycode.text == "" {
+            showAlert(title: "GameVPN", message: "Please enter country code")
+        }else if txtState.text == "" {
+            showAlert(title: "GameVPN", message: "Please enter state")
+        } else if txtIPAddress.text == "" {
+            showAlert(title: "GameVPN", message: "Please enter IPAddress")
+        } else if txtDomain.text == "" {
+            showAlert(title: "GameVPN", message: "Please enter domain")
+        } else if txtPassword.text == "" {
+            showAlert(title: "GameVPN", message: "Please enter password")
+        } else {
+            SVProgressHUD.show()
+            firebaseConnect.encryptPassword(password: txtPassword.text ?? "", vc: self) { encryptedPassword in
+                guard let encryptedPassword = encryptedPassword else {
+                    print("Encryption failed.")
+                    self.showAlert(title: "GameVPN", message: "Encryption failed.")
+                    return
+                }
+                self.firebaseConnect.SetNewServerDataToFirebase(country: self.txtContry.text ?? "", countryCode: self.txtContrycode.text ?? "", domain: self.txtDomain.text ?? "", ip: self.txtIPAddress.text ?? "", password: encryptedPassword, state: self.txtState.text ?? "", username: self.txtUser.text ?? "",  vc: self)
+            }
+        }
+        }
+    }
--- GameVPNFirebase/GameVPNFirebase/Entities/FirebaseConnect.swift
+++ GameVPNFirebase/GameVPNFirebase/Entities/FirebaseConnect.swift
@@ -14,7 +14,13 @@ import UIKit
 
 class FirebaseConnect : ObservableObject {
     static private var key: String = "0-qh#@,c^._PNmX5!]gpM_vHJfCP2POs"
-    public func encryptPassword(password: String, iPAddress: String,vc: ViewController, completion: @escaping (String?) -> Void) {
+    var ref: DatabaseReference!
+    
+    init() {
+        self.ref = Database.database().reference()
+    }
+    
+    public func encryptPassword(password: String,vc: UIViewController, completion: @escaping (String?) -> Void) {
         
         guard let keyData = FirebaseConnect.key.data(using: .utf8), let passwordData = password.data(using: .utf8) else {
             completion(nil)
@@ -48,7 +54,7 @@ class FirebaseConnect : ObservableObject {
         if UInt32(cryptStatus) == UInt32(kCCSuccess) {
             cryptData.count = numBytesEncrypted
             let encryptedString = cryptData.base64EncodedString()
-             completion(encryptedString)
+            completion(encryptedString)
         } else {
             completion(nil)
             SVProgressHUD.dismiss()
@@ -56,9 +62,8 @@ class FirebaseConnect : ObservableObject {
     }
     
     
-    public func updateUserIPAddressOnPasswordChange(iPAddress: String,  password: String,vc: ViewController) {
-        var ref: DatabaseReference!
-            ref = Database.database().reference()
+    public func updateUserIPAddressOnPasswordChange(iPAddress: String,  password: String,vc: UIViewController) {
+        
         let child = ref.child("serverDetails").child("payload").child("rows")
         
         // Query Firebase to find the server with the specified IP address
@@ -88,5 +93,84 @@ class FirebaseConnect : ObservableObject {
                 }
             }
         })
+    }
+    
+    func checkAppStatus(completion: @escaping ((Bool,String)->())) {
+        ref.child("appInfo").getData(completion: { error, snapshot in
+            guard error == nil else {
+                print(error!.localizedDescription)
+                completion(false, "")
+                return;
+            }
+            
+            if snapshot?.exists() ?? false {
+                if let value = snapshot?.value as? [String : Any] {
+                    if let update = value["inMaintenance"] as? Bool, update{
+                        completion(true, "App in maintenace mode")
+                    } else if let version = value["appVersion"] as? String{
+                        if version >= Constants.AppInfo.currentVersion{
+                            completion(true, "Please update the app")
+                        } else {
+                            completion(false, "")
+                        }
+                    }else {
+                        completion(false, "")
+                    }
+                }
+            } else {
+                completion(false, "")
+            }
+        })
+    }
+    
+    
+    func SetNewServerDataToFirebase(country: String,countryCode: String, domain: String,ip: String, password: String, state: String, username: String, vc: UIViewController) {
+        
+        let rowsRef = ref.child("serverDetails/payload/rows")
+        var nextId: Int = 1 // Default starting ID
+        // Example: Find the highest ID and increment it
+        rowsRef.observeSingleEvent(of: .value) { snapshot in
+            if let children = snapshot.children.allObjects as? [DataSnapshot] {
+                for child in children {
+                    if let id = Int(child.key), id >= nextId {
+                        nextId = id + 1
+                    }
+                }
+            }
+            
+            // Prepare new data with the dynamically calculated ID
+            let newData: [String: Any] = [
+                "country": country,
+                "countryCode": countryCode,
+                "createdAt": "\(Date())",
+                "deletedAt": "",
+                "domain": domain,
+                "id": nextId,
+                "ip": ip,
+                "isAndroid": 1,
+                "isIos": 1,
+                "isPremium": 1,
+                "password": password,
+                "state": state,
+                "status": 0,
+                "type": 0,
+                "updatedAt": "\(Date())",
+                "username": username
+            ]
+            
+            // Add the new data to Firebase
+            let newChildRef = rowsRef.child(String(nextId))
+            newChildRef.setValue(newData) { (error, ref) in
+                if let error = error {
+                    print("Error adding new data: \(error.localizedDescription)")
+                    vc.showAlert(title: "GameVPN", message: "\(error.localizedDescription)")
+                    SVProgressHUD.dismiss()
+                } else {
+                    print("New data added successfully!")
+                    vc.showAlert(title: "GameVPN", message: "New data added successfully!")
+                    SVProgressHUD.dismiss()
+                }
+            }
+        }
     }
 }
